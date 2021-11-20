ea28f151dbae132eaa6bba8ce051f9569ad5a0e2
valentina263 <yvcamayo@misena.edu.co>
2021-11-20 12:55:11 -0500

    repo0121

---
+++ js/Music.js
@@ -0,0 +1,29 @@
+function Music(Genero, Ritmo, Artista) {
+    this.Music = Music;
+}
+class Music {
+    constructor(Music = ("*")) {
+        this.music = Music;
+    }
+}
+
+class Genero {
+    constructor(Genero = ("El tipo de genero es:" + Genero)) {
+        this._nombre = nombre;
+        this.generos = [Pop,Hip-Hop];
+    }
+}
+
+class Artista {
+    constructor(Artista = ("El Artista es:" + Artista)) {
+        this.nombre = Artista;
+        this.edad = Artista;
+        this.apodo = Artista;
+        this.vive = true;
+        this.generos = [Pop,Hip-Hop];s
+    }
+}
+
+let Genero = new Genero('Instrumental');
+let Artista = new Artista('Travis Thompson');
+let Music = new Music('Instrumental');
